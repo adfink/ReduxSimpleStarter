91ed6bb60deefd247f391c770d3eb6a41cfdca29
Andrew Fink <andrewfink8@gmail.com>
2016-04-16 19:04:01 -0700

    can click on and view any of the five videos. onVideoSelect callback working as should

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const VideoDetail = ({video}) => {
+const VideoDetail = (props) => {
+  const video = props.video
+  
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
   const videoId = video.id.videoId;
   const url = `https:/www.youtube.com/embed/${videoId}`;
   return (
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 import SearchBar from './components/search_bar';
 
@@ -9,25 +10,32 @@ import SearchBar from './components/search_bar';
 const API_KEY = "AIzaSyCoBIwwOPteQYC9c3Dqcj6ZlALpzfjY0C0";
 
 
-
-// create a new component that should produce some html
-
 class App extends Component {
 
     constructor(props){
       super(props);
-      this.state = { videos: [] };
+      this.state = { videos: [],
+                     selectedVideo: null
+                   };
 
+      
       YTSearch({key: API_KEY, term: 'surfing'}, (videos) => {
-        this.setState({ videos });
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+         });
       });
 
     }
+
     render(){
       return(
         <div>
             <SearchBar />
-            <VideoList videos={this.state.videos} />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList
+              onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+              videos={this.state.videos} />
         </div>
 
       );
