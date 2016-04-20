ef873ae20c1581495375e8778b2843055efcb2e4
Andrew Fink <andrewfink8@gmail.com>
2016-04-19 19:34:09 -0700

    can now search for new videos

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -16,10 +16,15 @@ class SearchBar extends React.Component {
       <div className="search-bar">
         <input
           value = {this.state.term}
-         onChange={ event =>  this.setState({term: event.target.value})} />
+         onChange={ event =>  this.onInputChange(event.target.value)} />
          The current state term is : {this.state.term}
       </div>
    )
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term); 
   }
 
 
